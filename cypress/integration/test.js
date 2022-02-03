41a442b0774d294a44b7813dc975a0137839e0bb
Jose <jose_esteban_basualdo@hotmail.com>
2022-02-03 13:32:23 -0300

    adds tries test and random card test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -1,82 +1,171 @@
-   before(() => {
-    cy.visit('http://192.168.1.38:8080');
+  context ('Memotest', ()=>{
 
-  }); 
-
-  describe ('Game first load test', ()=>{
-
-
-    it ('checks if start button is enabled', ()=>{
-
-      cy.get('#start-btn')
-      .should('be.enabled')
-
-
+    before(() => {
+      cy.visit('http://192.168.1.38:8080');
+  
+    }); 
+  
+    describe ('Game first load test', ()=>{
+  
+  
+      it ('checks if start button is enabled', ()=>{
+  
+        cy.get('#start-btn')
+        .should('be.enabled')
+  
+  
+      })
+  
+      it('checks if timer and tries values are empty',()=>{
+  
+        cy.get('#timer').should('have.value','')
+        cy.get('#tries').should('have.value',"")
+  
+  
+      })
+  
+      it('checks is win image is hidden',()=>{
+  
+        cy.get('#win-img').should('not.be.visible')
+  
+      })
+  
+      it('checks if welcome message has the correct text',()=>{
+  
+        cy.get('#welcome-state').should('have.text','Apreta el botón comenzar para jugar!')
+  
+      })
+  
+  
+  
+  
+  
     })
-
-    it('checks if timer and tries values are empty',()=>{
-
-      cy.get('#timer').should('have.value','')
-      cy.get('#tries').should('have.value',"")
-
-
-    })
-
-    it('checks is win image is hidden',()=>{
-
-      cy.get('#win-img').should('not.be.visible')
-
-    })
-
-    it('checks if welcome message has the correct text',()=>{
-
-      cy.get('#welcome-state').should('have.text','Apreta el botón comenzar para jugar!')
-
-    })
-
-
-
-
-
-  })
-
-
-   describe('starts the game', () => {
-    it('checks if start button is disabled after starting the game', () => {
-      
-      //cy.contains('#start-btn')
-       //cy.get('#start-btn').click().should('include','disabled') 
-      
-     cy.get('#start-btn').click();
-
-     cy.get('#start-btn').should('have.class', 'disabled')
-      
-      //expect(cy.get('#start-btn')).to.be.disabled
+  
+  
+     describe('starts the game', () => {
+  
      
+  
+      
+      it('checks if start button is disabled after starting the game', () => {
+        
+        //cy.contains('#start-btn')
+         //cy.get('#start-btn').click().should('include','disabled') 
+        
+       cy.get('#start-btn').click();
+  
+       cy.get('#start-btn').should('have.class', 'disabled')
+        
+        //expect(cy.get('#start-btn')).to.be.disabled
+       
+      })
+  
+      it('checks if the game board is created',()=>{
+  
+        cy.get('.card-back').should('have.length', 16)
+  
+  
+      })
+  
+      it('checks if timers is running',()=>{
+  
+        let timer = cy.get('#timer')
+  
+        expect(timer).to.not.equal(0)
+  
+  
+  
+      })
+  
+  
     })
 
-    it('checks if the game board is created',()=>{
 
-      cy.get('.card-back').should('have.length', 16)
+    describe('Plays the game ',()=>{
+
+      const numberOfCards = 16
+
+
+      it('checks gameBoard length',()=>{
+        cy.get('#game-board').find('.card-back').should('have.length',numberOfCards)
+      })
+
+      it('checks that cards are randomly set',()=>{
+
+        let firstCreatedCards =[]
+        let secondlyCreatedCards =[]
+        cy.get('.card-back').then((cards)=>{
+
+         
+
+          cards.each((i,card)=>{
+
+            firstCreatedCards.push(card.children)
+            
+          })
+
+
+
+        });
+
+        cy.visit('http://192.168.1.38:8080')
+
+        cy.get('#start-btn').click()
+
+        cy.get('.card-back').then((cards)=>{
+
+         
+
+          cards.each((i,card)=>{
+
+            secondlyCreatedCards.push(card.children)
+            
+          })
+
+
+
+
+        });
+
+
+        expect(firstCreatedCards).to.not.equal(secondlyCreatedCards)
+
+
+
+      })
+
+
+      it('checks tries', ()=>{
+  
+        cy.get('#1').click()
+        cy.get('#2').click()
+        .then(()=>{
+
+          cy.get('#tries').should('have.text',' Intentos: 1 ')
+
+        })
+        
+   
+       })
+
+       
+       //TESTEAR UN MATCH
 
 
     })
-
-    it('checks if timers is running',()=>{
-
-      let timer = cy.get('#timer')
-
-      expect(timer).to.not.equal(0)
-
-
-
-    })
-
+  
 
 
   })
 
 
- 
 
 
+  function getCardPairs(){
+
+
+
+
+
+  }
